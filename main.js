5f0bfa4d0a226c443510bc40703d22252d059a64
proxza <proxza@gmail.com>
2024-07-06 15:24:17 +1000

    Start adding traffic

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ canvas.width = 200;
 // car
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 4);
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "BOT", 3)];
 
 // start animation
 animate();
 
 function animate() {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders);
+  }
+
   car.update(road.borders);
 
   canvas.height = window.innerHeight;
@@ -18,6 +23,11 @@ function animate() {
   ctx.translate(0, -car.y + canvas.height * 0.7);
 
   road.draw(ctx);
+
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx);
+  }
+
   car.draw(ctx);
 
   ctx.restore();
